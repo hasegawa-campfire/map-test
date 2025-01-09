bc9c2aea405926e1f408a11bbd257b8421770a6a
koichi.hasegawa <koichi.hasegawa@camp-fire.jp>
2025-01-09 18:53:47 +0900

    bin to json

--- src/lib/projects.svelte.ts
+++ src/lib/projects.svelte.ts
@@ -1,14 +1,19 @@
 import { browser } from '$app/environment'
 import type { Project } from '$lib/types'
 
-async function fetchBin() {
-  const res = await fetch('./projects.bin')
-  if (!res.ok || !res.body) throw Error()
-  const ds = new DecompressionStream('gzip')
-  const stream = res.body.pipeThrough(ds)
-  return new Response(stream).json()
+// async function fetchData() {
+//   const res = await fetch('./projects.bin')
+//   if (!res.ok || !res.body) throw Error()
+//   const ds = new DecompressionStream('gzip')
+//   const stream = res.body.pipeThrough(ds)
+//   return new Response(stream).json()
+// }
+
+async function fetchData() {
+  const res = await fetch('./projects.json')
+  return res.json()
 }
 
-const promise: Promise<Project[]> = browser ? fetchBin() : Promise.resolve([])
+const promise: Promise<Project[]> = browser ? fetchData() : Promise.resolve([])
 
 export default promise
